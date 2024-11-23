3b21cafb093fe8df7020470eb3aa200640afa27e
akshithamamidala <akshithamamidala25@gmail.com>
2024-11-23 21:25:46 +0530

    Fix port binding for deployment

--- scripts/flask_api.py
+++ scripts/flask_api.py
@@ -46,4 +46,6 @@ def predict():
         return jsonify({"error": f"An error occurred: {e}"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
